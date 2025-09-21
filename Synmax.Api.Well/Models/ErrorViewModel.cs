caab6668011b64d98ea459834c065a8dd013b4d6
Nicholas Finch <ngf4@nau.edu>
2025-09-21 11:28:31 -0400

    Initial Commit

---
+++ Synmax.Api.Well/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Synmax.Api.Well.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
